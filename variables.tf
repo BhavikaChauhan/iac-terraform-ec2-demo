61de8269af75440e92c4d671e0e14cd41c1d693f
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-07-17 18:34:20 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Name of the existing AWS key pair"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
