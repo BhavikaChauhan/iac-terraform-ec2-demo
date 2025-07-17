99bc1284f897d0500cb493835d24529794164111
Bhavika Chauhan <137499857+BhavikaChauhan@users.noreply.github.com>
2025-07-17 22:26:38 +0530

    Updates

--- main.tf
+++ main.tf
@@ -1,23 +1,24 @@
-resource "aws_instance" "web_server" {
-  ami                         = "ami-020cba7c55df1f615"
-  instance_type               = var.instance_type
-  key_name                    = "awskey"
-  associate_public_ip_address = true
+ resource "aws_instance" "web_server" {
+   ami                         = "ami-020cba7c55df1f615"
+   instance_type               = var.instance_type
+   key_name                    = "awskey"
+   associate_public_ip_address = true
 
-  vpc_security_group_ids = [
-    aws_security_group.web_sg.id
-  ]
+   vpc_security_group_ids = [
+     aws_security_group.web_sg.id
+   ]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    amazon-linux-extras install nginx1 -y
-    systemctl start nginx
-    systemctl enable nginx
-    echo "<h1>Terraform EC2 Demo by Bhavika</h1>" > /usr/share/nginx/html/index.html
-  EOF
+-  user_data = <<-EOF
+-    #!/bin/bash
+-    apt-get update -y
+-    apt-get install nginx -y
+-    systemctl start nginx
+-    systemctl enable nginx
+-    echo "<h1>Terraform EC2 Demo by Bhavika</h1>" > /var/www/html/index.nginx-debian.html
+-  EOF
 
-  tags = {
-    Name = "Terraform-Web-Server"
-  }
-}
+   tags = {
+     Name = "Terraform-Web-Server"
+   }
+ }
+
